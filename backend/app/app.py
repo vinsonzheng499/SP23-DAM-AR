62968e2c3617276f4c63761a7b8f1b557d261520
rohitchawla28 <rohitchawla77@gmail.com>
2023-03-08 19:39:31 -0600

    more work on app.py routing

--- backend/app/app.py
+++ backend/app/app.py
@@ -15,7 +15,7 @@ def get_artwork(art_id):
     }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute('SELECT FROM art_table')
+    cursor.execute(f'SELECT * FROM art_table WHERE art_id = {art_id}')
     results = [{name: role} for (id, name, role) in cursor]
     cursor.close()
     connection.close()
@@ -23,6 +23,14 @@ def get_artwork(art_id):
     return results
 
 # route to get specific artist info
+
+@app.route('/')
+def index() -> str:
+    return json.dumps('hi')
+
+@app.route('/artwork/<id>')
+def artinfo(id) -> str:
+    return json.dumps(get_artwork(id))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
