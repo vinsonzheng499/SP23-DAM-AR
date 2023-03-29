ac62fb5be6d15c98949378ef367063c2c2a399cd
vinsonzheng499 <vinsonzheng499@gmail.com>
2023-03-29 18:41:27 -0500

    Added android support

--- ARProject/IDScannerPage.js
+++ ARProject/IDScannerPage.js
@@ -1,15 +1,9 @@
-//this is a barebones scanner page
-//in which the user inputs an integer
-//for the id of the art work they want to retrieve
-//from the sql database, and then it should go to the page
-//with the art work's information. this is a lower level
-//version before we actually implemented it with NFC tags
-import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
-import ScanPageSample from './ScanPageSample';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { NfcManager, NfcTech } from 'react-native-nfc-manager';
 import * as Font from 'expo-font';
 
-export default function ArtworkSearch({ navigation }) {
+export default function IDScannerPage({ navigation }) {
   const [artworkID, setArtworkID] = useState('');
 
   const handlePress = () => {
@@ -17,29 +11,61 @@ export default function ArtworkSearch({ navigation }) {
     // and then navigate to the artwork information page
     // TODO: Backend
 
-    //navigation.navigate('ArtworkInformation', { artworkID: artworkID });
-    navigation.navigate('ScanPageSample');
+    navigation.navigate('ArtworkInformation', { artworkID: artworkID });
   }
 
   const [fontLoaded, setFontLoaded] = useState(false);
 
+  // Initialize the NFC manager
+  useEffect(() => {
+    NfcManager.start();
+    return () => {
+      NfcManager.cancelTechnologyRequest().catch(() => 0);
+      NfcManager.unregisterTagEvent().catch(() => 0);
+      NfcManager.stop();
+    };
+  }, []);
+
+  // Handle scanned NFC tag
+  const handleNfcDiscover = async (tag) => {
+    const tech = NfcTech.Ndef;
+    const result = await NfcManager.requestTechnology(tech, {
+      alertMessage: 'Ready to read an NFC tag'
+    });
+    const ndef = await NfcManager.getNdefMessage();
+    if (ndef) {
+      const id = ndef[0].payload;
+      setArtworkID(id);
+      handlePress();
+    }
+    await NfcManager.closeTechnology(tech);
+  };
+
+  // Enable foreground dispatch for NFC tag scanning
+  useEffect(() => {
+    const enableForegroundDispatch = async () => {
+      try {
+        await NfcManager.registerTagEvent(handleNfcDiscover);
+      } catch (ex) {
+        console.warn('Error enabling NFC foreground dispatch', ex);
+      }
+    };
+    enableForegroundDispatch();
+    return () => {
+      NfcManager.unregisterTagEvent().catch(() => 0);
+    };
+  }, []);
+
+  if (!fontLoaded) {
+    return null;
+  }
 
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>Search for an Artwork</Text>
-
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Artwork ID:</Text>
-        <TextInput
-          style={styles.input}
-          value={artworkID}
-          onChangeText={setArtworkID}
-          keyboardType="number-pad"
-        />
-      </View>
+      <Text style={styles.title}>Scan or Search for an Artwork</Text>
 
       <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Submit</Text>
+        <Text style={styles.buttonText}>Search by ID</Text>
       </TouchableOpacity>
     </View>
   );
@@ -59,28 +85,6 @@ const styles = StyleSheet.create({
     color: '#E4A21D',
     fontFamily: 'Helvetica Neue',
     letterSpacing: 2,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginBottom: 20,
-  },
-  label: {
-    fontSize: 24,
-    marginRight: 10,
-    color: '#EFEFEF',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 1,
-  },
-  input: {
-    flex: 1,
-    backgroundColor: '#EFEFEF',
-    height: 40,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    fontFamily: 'Helvetica Neue',
-    fontSize: 20,
-    letterSpacing: 1,
   },
   button: {
     backgroundColor: '#E4A21D',
