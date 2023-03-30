ca750afd8d1e073d4d9f49954d14ed758d1adbe7
rohitchawla28 <rohitchawla77@gmail.com>
2023-03-30 02:44:47 -0500

    artwork route works (tested dumping mona lisa data), need to uncomment other table functionality and other routes and begin testing that

--- backend/app/app.py
+++ backend/app/app.py
@@ -1,91 +1,131 @@
-from flask import Flask, jsonify, request
+from typing import List, Dict
+# from flask import Flask, jsonify, request
+from flask import Flask
 import mysql.connector
 import json
 
 # Set up a connection to the MySQL database
-mydb = mysql.connector.connect(
-  host = "db",
-  port = "3306",
-  user = "root",
-  password = "root",
-  database = "art"
-)
+# mydb = mysql.connector.connect(
+#   host = "db",
+#   port = "3306",
+#   user = "root",
+#   password = "root",
+#   database = "art"
+# )
 
 app = Flask(__name__)
 
 # route to get specific artwork info
-def get_artwork(art_id):
-    cursor = mydb.cursor()
-    cursor.execute(f'SELECT * FROM art_table WHERE art_id = {art_id}')
-    result = cursor.fetchone()
-    cursor.close()
-
-    if result is None:
-        return {'error': f'Artwork with ID {art_id} not found'}
-
-    return {
-        'art_id': result[0],
-        'artist_id': result[1],
-        'art_name': result[2],
-        'museum_name': result[3],
-        'art_date': result[4].isoformat(),
-        'art_era': result[5],
-        'art_type': result[6],
-        'image_id': result[7],
-        'art_description': result[8],
-        'audio_file_id': result[9]
+def get_artwork(art_id) -> List[str]:
+    config = {
+        'user': 'root',
+        'password': 'root',
+        'host': 'db',
+        'port': '3306',
+        'database': 'art'
     }
+    connection = mysql.connector.connect(**config)
+    cursor = connection.cursor()
+    cursor.execute(f'SELECT * FROM art_table WHERE art_id = {art_id}')
+
+    # want results to be List of strings
+    # results = [{art_id: art_name} for (art_id, artist_id, art_name) in cursor]
+
+    results = []
+    for row in cursor:
+        # date type can be converted?
+        rowStr = f'art_id: {row[0]}, art_name: {row[2]}, art_date: {row[4]}, art_era: {row[5]}, image_id: {row[7]}, art_desc: {row[8]}, audio_id: {row[9]}'
+        results.append(rowStr)
+
+    cursor.close()
+    connection.close()
+
+    return results
+
+
+    # # tag_data = request.args.get('tagData')
+    # cursor = mydb.cursor()
+    # cursor.execute(f'SELECT * FROM art_table WHERE art_id = {art_id}')
+    # # cursor.execute(f'SELECT * FROM art_table WHERE art_id = {tag_data}')
+    # result = cursor.fetchone()
+    # cursor.close()
+
+    # if result is None:
+    #     return {'error': f'Artwork with ID {art_id} not found'}
+
+    # return {
+    #     'art_id': result[0]
+    #     # 'artist_id': result[1],
+    #     # 'art_name': result[2],
+    #     # 'museum_name': result[3],
+    #     # 'art_date': result[4].isoformat(),
+    #     # 'art_era': result[5],
+    #     # 'art_type': result[6],
+    #     # 'image_id': result[7],
+    #     # 'art_description': result[8],
+    #     # 'audio_file_id': result[9]
+    # }
 
 # route to get specific artist info
-def get_artist(artist_id):
-    cursor = mydb.cursor()
-    cursor.execute(f'SELECT * FROM artist_table WHERE artist_id = {artist_id}')
-    result = cursor.fetchone()
-    cursor.close()
+# def get_artist(artist_id):
+#     cursor = mydb.cursor()
+#     cursor.execute(f'SELECT * FROM artist_table WHERE artist_id = {artist_id}')
+#     result = cursor.fetchone()
+#     cursor.close()
 
-    if result is None:
-        return {'error': f'Artwork with ID {artist_id} not found'}
+#     if result is None:
+#         return {'error': f'Artwork with ID {artist_id} not found'}
 
-    return {
-        'artist_id': result[0],
-        'artist_name': result[1],
-        'art_lifespan': result[2],
-        'image_id': result[3],
-        'biography': result[4],
-        'audio_file_id': result[5],
-    }
+#     return {
+#         'artist_id': result[0],
+#         'artist_name': result[1],
+#         'art_lifespan': result[2],
+#         'image_id': result[3],
+#         'biography': result[4],
+#         'audio_file_id': result[5],
+#     }
 
 # Define a route to return the audio file for a given piece of art
-@app.route('/art/<int:art_id>/audio', methods=['GET'])
-def get_art_audio(art_id):
-    # Query the art_table to get the audio_file_id for the given art_id
-    cursor = mydb.cursor()
-    query = "SELECT audio_file_id FROM art_table WHERE art_id = %s"
-    cursor.execute(query, (art_id,))
-    result = cursor.fetchone()
-    if not result:
-        return jsonify({'error': 'Art not found'}), 404
-    audio_file_id = result[0]
+# @app.route('/art/<int:art_id>/audio', methods=['GET'])
+# def get_art_audio(art_id):
+#     # Query the art_table to get the audio_file_id for the given art_id
+#     cursor = mydb.cursor()
+#     query = "SELECT audio_file_id FROM art_table WHERE art_id = %s"
+#     cursor.execute(query, (art_id,))
+#     result = cursor.fetchone()
+#     if not result:
+#         return jsonify({'error': 'Art not found'}), 404
+#     audio_file_id = result[0]
     
-    # Query the audio_table to get the audio file address for the retrieved audio_file_id
-    query = "SELECT audio_file_address FROM audio_table WHERE audio_file_id = %s"
-    cursor.execute(query, (audio_file_id,))
-    result = cursor.fetchone()
-    if not result:
-        return jsonify({'error': 'Audio file not found'}), 404
-    audio_file_address = result[0]
+#     # Query the audio_table to get the audio file address for the retrieved audio_file_id
+#     query = "SELECT audio_file_address FROM audio_table WHERE audio_file_id = %s"
+#     cursor.execute(query, (audio_file_id,))
+#     result = cursor.fetchone()
+#     if not result:
+#         return jsonify({'error': 'Audio file not found'}), 404
+#     audio_file_address = result[0]
     
-    # TODO: Return the audio file located at the retrieved audio_file_address
-    # Retrieve the audio file from the given address and return it in the appropriate format
-    return jsonify({'audio_file_address': audio_file_address}), 200
+#     # TODO: Return the audio file located at the retrieved audio_file_address
+#     # Retrieve the audio file from the given address and return it in the appropriate format
+#     return jsonify({'audio_file_address': audio_file_address}), 200
 
 @app.route('/')
 def index() -> str:
-    return json.dumps('hi')
+    return json.dumps('hihi')
 
 @app.route('/artwork/<id>')
 def artinfo(id) -> str:
     return json.dumps(get_artwork(id))
 
+# @app.route('/artist/<int:id>')
+# def artistinfo(id) -> str:
+#     return json.dumps(get_artist(id))
+
 if __name__ == '__main__':
+    app.debug = True
     app.run(host='0.0.0.0')
+
+# docker-compose build
+# docker-compose up
+
+# localhost:5000/
--- backend/db/init.sql
+++ backend/db/init.sql
@@ -12,51 +12,52 @@ CREATE TABLE art_table (
   art_type VARCHAR(20),
   image_id INT(10),
   art_description MEDIUMTEXT,
-  audio_file_id INT(10),
+  audio_file_id INT(10)
 
-  PRIMARY KEY (art_id),
-  FOREIGN KEY (artist_id) REFERENCES artist_table(artist_id),
-  FOREIGN KEY (image_id) REFERENCES image_table(image_id),
-  FOREIGN KEY (audio_file_id) REFERENCES audio_table(audio_file_id)
+  -- PRIMARY KEY (art_id)
+  -- FOREIGN KEY (artist_id) REFERENCES artist_table(artist_id),
+  -- FOREIGN KEY (image_id) REFERENCES image_table(image_id),
+  -- FOREIGN KEY (audio_file_id) REFERENCES audio_table(audio_file_id)
 );
 
 INSERT INTO art_table
   (art_id, artist_id, art_name, museum_name, art_date, art_era, art_type, image_id, art_description, audio_file_id)
+  -- (art_id, artist_id, art_name)
 VALUES
-  (1, 1, 'Mona lisa', 'Louvre Museum', '1503-03-28', 'Renaissance', 'Painting', 1, 'Nice work.', 1);
+  (1, 2, 'Mona lisa', 'Louvre Museum', '1503-03-28', 'Renaissance', 'Painting', 1, 'Nice work.', 1);
 
 
-CREATE TABLE artist_table (
-  artist_id INT(10),
-  artist_name VARCHAR(50),
-  artist_lifespan VARCHAR(50),
-  image_id INT(10),
-  biography MEDIUMTEXT,
-  audio_file_id INT(10),
+-- CREATE TABLE artist_table (
+--   artist_id INT(10),
+--   artist_name VARCHAR(50),
+--   artist_lifespan VARCHAR(50),
+--   image_id INT(10),
+--   biography MEDIUMTEXT,
+--   audio_file_id INT(10),
   
-  PRIMARY KEY (artist_id),
-  FOREIGN KEY (image_id) REFERENCES image_table(image_id),
-  FOREIGN KEY (audio_file_id) REFERENCES audio_table(audio_file_id)
-);
+--   PRIMARY KEY (artist_id),
+--   FOREIGN KEY (image_id) REFERENCES image_table(image_id),
+--   FOREIGN KEY (audio_file_id) REFERENCES audio_table(audio_file_id)
+-- );
 
-INSERT INTO artist_table
-  (artist_id, artist_name, artist_lifespan, image_id, biography, audio_file_id)
-VALUES
-  (1, 'Da Vinci', '1452-04-15 to 1519-05-02', 2, 'Great guy.', 2);
+-- INSERT INTO artist_table
+--   (artist_id, artist_name, artist_lifespan, image_id, biography, audio_file_id)
+-- VALUES
+--   (1, 'Da Vinci', '1452-04-15 to 1519-05-02', 2, 'Great guy.', 2);
 
 
-CREATE TABLE image_table (
-  image_id INT(10),
-  image_blob BLOB, --do i need size? why is blob not recognized atm?
+-- CREATE TABLE image_table (
+--   image_id INT(10),
+--   image_blob BLOB, --do i need size? why is blob not recognized atm?
 
-  PRIMARY KEY (image_id)
-);
-INSERT INTO MyTable (image) VALUES(LOAD_FILE('/tmp/image.png'));
-INSERT INTO image_table
-  (image_id, image_blob)
-VALUES
-  (1, LOAD_FILE('C:\Users\15125\SP23-DAM-AR\backend\Assets\MonaLisa.jpg'));
-  -- (2, /*da vinci img*/);
+--   PRIMARY KEY (image_id)
+-- );
+-- INSERT INTO MyTable (image) VALUES(LOAD_FILE('/tmp/image.png'));
+-- INSERT INTO image_table
+--   (image_id, image_blob)
+-- VALUES
+--   (1, LOAD_FILE('C:\Users\15125\SP23-DAM-AR\backend\Assets\MonaLisa.jpg'));
+--   -- (2, /*da vinci img*/);
 
 
 -- CREATE TABLE audio_table (
