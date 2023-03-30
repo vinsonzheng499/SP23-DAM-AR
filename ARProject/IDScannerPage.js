dc709c1b0d55e8b6849ec5193fb8eef046859fc6
Zayaan <72232563+grizzledwarvet23@users.noreply.github.com>
2023-03-29 19:08:11 -0500

    works too

--- ARProject/IDScannerPage.js
+++ ARProject/IDScannerPage.js
@@ -5,7 +5,7 @@
 //with the art work's information. this is a lower level
 //version before we actually implemented it with NFC tags
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Image, Text, View, TextInput, TouchableOpacity, ImageBackground } from 'react-native';
 import ScanPageSample from './ScanPageSample';
 import * as Font from 'expo-font';
 
@@ -16,7 +16,8 @@ import * as Font from 'expo-font';
 export default function IDScannerPage({ navigation }) {
   const [artworkID, setArtworkID] = useState('');
   const [fontLoaded, setFontLoaded] = useState(false);
-
+  
+  const backgroundImage = require('./background-image.png');
   //nfc manager stuff
   // useEffect(() => {
   //   NfcManager.start();
@@ -52,74 +53,136 @@ export default function IDScannerPage({ navigation }) {
 
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Search for an Artwork</Text>
+    // <View style={styles.container}>
+    //   <Text style={styles.title}>Search for an Artwork</Text>
 
-      <View style={styles.inputContainer}>
-        <Text style={styles.label}>Artwork ID:</Text>
-        <TextInput
-          style={styles.input}
-          value={artworkID}
-          onChangeText={setArtworkID}
-          keyboardType="number-pad"
-        />
+    //   <View style={styles.inputContainer}>
+    //     <Text style={styles.label}>Artwork ID:</Text>
+    //     <TextInput
+    //       style={styles.input}
+    //       value={artworkID}
+    //       onChangeText={setArtworkID}
+    //       keyboardType="number-pad"
+    //     />
+    //   </View>
+
+    //   <TouchableOpacity style={styles.button} onPress={handlePress}>
+    //     <Text style={styles.buttonText}>Submit</Text>
+    //   </TouchableOpacity>
+    // </View>
+    <ImageBackground source={backgroundImage} style={styles.background}>
+    <View style={styles.rectangle}>
+      <Text style={styles.title}>Ready to Scan</Text>
+      <View style={styles.logoContainer}>
+        <Image source={require('./phone-logo.png')} style={styles.logo} />
       </View>
-
-      <TouchableOpacity style={styles.button} onPress={handlePress}>
-        <Text style={styles.buttonText}>Submit</Text>
+      <Text style={styles.description}>Hold your iPhone near the item to learn more about it.</Text>
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Cancel</Text>
       </TouchableOpacity>
     </View>
+  </ImageBackground>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  background: {
     flex: 1,
-    backgroundColor: '#121212',
+    resizeMode: 'cover',
+  },
+  rectangle: {
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    right: 0,
+    height: '50%',
+    backgroundColor: 'white',
+    borderTopLeftRadius: 40,
+    borderTopRightRadius: 40,
     alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
   },
   title: {
-    fontSize: 36,
+    fontSize: 30,
     fontWeight: 'bold',
-    marginBottom: 40,
-    color: '#E4A21D',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 2,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
     marginBottom: 20,
   },
-  label: {
-    fontSize: 24,
-    marginRight: 10,
-    color: '#EFEFEF',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 1,
+  logoContainer: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    backgroundColor: 'lightgray',
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginBottom: 20,
   },
-  input: {
-    flex: 1,
-    backgroundColor: '#EFEFEF',
-    height: 40,
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    fontFamily: 'Helvetica Neue',
+  logo: {
+    width: 50,
+    height: 50,
+  },
+  description: {
     fontSize: 20,
-    letterSpacing: 1,
+    textAlign: 'center',
+    marginBottom: 20,
   },
   button: {
-    backgroundColor: '#E4A21D',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
+    backgroundColor: 'green',
     borderRadius: 10,
+    padding: 10,
   },
   buttonText: {
-    fontSize: 24,
+    color: 'white',
+    fontSize: 20,
     fontWeight: 'bold',
-    color: '#121212',
-    fontFamily: 'Helvetica Neue',
-    letterSpacing: 2,
   },
+  // container: {
+  //   flex: 1,
+  //   backgroundColor: '#121212',
+  //   alignItems: 'center',
+  //   justifyContent: 'center',
+  // },
+  // title: {
+  //   fontSize: 36,
+  //   fontWeight: 'bold',
+  //   marginBottom: 40,
+  //   color: '#E4A21D',
+  //   fontFamily: 'Helvetica Neue',
+  //   letterSpacing: 2,
+  // },
+  // inputContainer: {
+  //   flexDirection: 'row',
+  //   alignItems: 'center',
+  //   marginBottom: 20,
+  // },
+  // label: {
+  //   fontSize: 24,
+  //   marginRight: 10,
+  //   color: '#EFEFEF',
+  //   fontFamily: 'Helvetica Neue',
+  //   letterSpacing: 1,
+  // },
+  // input: {
+  //   flex: 1,
+  //   backgroundColor: '#EFEFEF',
+  //   height: 40,
+  //   borderRadius: 10,
+  //   paddingHorizontal: 10,
+  //   fontFamily: 'Helvetica Neue',
+  //   fontSize: 20,
+  //   letterSpacing: 1,
+  // },
+  // button: {
+  //   backgroundColor: '#E4A21D',
+  //   paddingVertical: 10,
+  //   paddingHorizontal: 20,
+  //   borderRadius: 10,
+  // },
+  // buttonText: {
+  //   fontSize: 24,
+  //   fontWeight: 'bold',
+  //   color: '#121212',
+  //   fontFamily: 'Helvetica Neue',
+  //   letterSpacing: 2,
+  // },
 });
