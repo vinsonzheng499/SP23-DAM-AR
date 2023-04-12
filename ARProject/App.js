336344bd99ad160b061a1dbda3a80cde97a7699c
vinsonzheng499 <vinsonzheng499@gmail.com>
2023-04-11 19:28:19 -0500

    Merging branches

--- ARProject/App.js
+++ ARProject/App.js
@@ -1,51 +1,93 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
-import HomePage from './HomePage'
-import ScanPageSample from './ScanPageSample';
-import IDScannerPage from './IDScannerPage';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import { Ionicons } from '@expo/vector-icons';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+// import HomePage from './HomePage'
+// import ScanPageSample from './ScanPageSample';
+// import IDScannerPage from './IDScannerPage';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createStackNavigator } from '@react-navigation/stack';
+// import { Ionicons } from '@expo/vector-icons';
 
-export default function App() {
-  const Stack = createStackNavigator();
+// export default function App() {
+//   const Stack = createStackNavigator();
   
+//   return (
+//     <NavigationContainer>
+//       <Stack.Navigator initialRouteName="HomePage">
+//         <Stack.Screen name="HomePage" component={HomePage} />
+//         <Stack.Screen name="IDScannerPage" component={IDScannerPage}/>
+//         <Stack.Screen
+//           name="ScanPageSample"
+//           component={ScanPageSample}
+//           options={({ navigation }) => ({
+//             headerLeft: () => (
+//               <TouchableOpacity
+//                 style={styles.headerButton}
+//                 onPress={() => navigation.navigate('HomePage')}
+//               >
+//                 <Ionicons name="close-outline" size={30} color="white" />
+//               </TouchableOpacity>
+//             ),
+//             headerTitle: '',
+//             headerStyle: {
+//               backgroundColor: '#121212',
+//             },
+//           })}
+//         />
+//       </Stack.Navigator>
+//     </NavigationContainer>
+//   );
+// }
+
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#000',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+//   headerButton: {
+//     marginLeft: 20,
+//   },
+// });
+
+import React from 'react';
+import {View, Text, TouchableOpacity, StyleSheet} from 'react-native';
+import NfcManager, {NfcTech} from 'react-native-nfc-manager';
+
+// Pre-step, call this before any NFC operations
+NfcManager.start();
+
+function App() {
+  async function readNdef() {
+    try {
+      // register for the NFC tag with NDEF in it
+      await NfcManager.requestTechnology(NfcTech.Ndef);
+      // the resolved tag object will contain `ndefMessage` property
+      const tag = await NfcManager.getTag();
+      console.warn('Tag found', tag);
+    } catch (ex) {
+      console.warn('Oops!', ex);
+    } finally {
+      // stop the nfc scanning
+      NfcManager.cancelTechnologyRequest();
+    }
+  }
+
   return (
-    <NavigationContainer>
-      <Stack.Navigator initialRouteName="HomePage">
-        <Stack.Screen name="HomePage" component={HomePage} />
-        <Stack.Screen name="IDScannerPage" component={IDScannerPage}/>
-        <Stack.Screen
-          name="ScanPageSample"
-          component={ScanPageSample}
-          options={({ navigation }) => ({
-            headerLeft: () => (
-              <TouchableOpacity
-                style={styles.headerButton}
-                onPress={() => navigation.navigate('HomePage')}
-              >
-                <Ionicons name="close-outline" size={30} color="white" />
-              </TouchableOpacity>
-            ),
-            headerTitle: '',
-            headerStyle: {
-              backgroundColor: '#121212',
-            },
-          })}
-        />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <View style={styles.wrapper}>
+      <TouchableOpacity onPress={readNdef}>
+        <Text>Scan a Tag</Text>
+      </TouchableOpacity>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  wrapper: {
     flex: 1,
-    backgroundColor: '#000',
-    alignItems: 'center',
     justifyContent: 'center',
-  },
-  headerButton: {
-    marginLeft: 20,
+    alignItems: 'center',
   },
 });
+
+export default App;
--- ARProject/IDScannerPage.js
+++ ARProject/IDScannerPage.js
@@ -4,61 +4,61 @@ import { NfcManager, NfcTech } from 'react-native-nfc-manager';
 import * as Font from 'expo-font';
 
 export default function IDScannerPage({ navigation }) {
-  const [artworkID, setArtworkID] = useState('');
+   const [artworkID, setArtworkID] = useState('');
 
   const handlePress = () => {
     // Here you can make a network request to your backend to retrieve the artwork data
     // and then navigate to the artwork information page
     // TODO: Backend
 
-    navigation.navigate('ArtworkInformation', { artworkID: artworkID });
+    navigation.navigate('ScanPageSample', { artworkID: artworkID });
   }
 
   const [fontLoaded, setFontLoaded] = useState(false);
 
-  // Initialize the NFC manager
-  useEffect(() => {
-    NfcManager.start();
-    return () => {
-      NfcManager.cancelTechnologyRequest().catch(() => 0);
-      NfcManager.unregisterTagEvent().catch(() => 0);
-      NfcManager.stop();
-    };
-  }, []);
+  // // Initialize the NFC manager
+  // useEffect(() => {
+  //   NfcManager.start();
+  //   return () => {
+  //     NfcManager.cancelTechnologyRequest().catch(() => 0);
+  //     NfcManager.unregisterTagEvent().catch(() => 0);
+  //     NfcManager.stop();
+  //   };
+  // }, []);
 
-  // Handle scanned NFC tag
-  const handleNfcDiscover = async (tag) => {
-    const tech = NfcTech.Ndef;
-    const result = await NfcManager.requestTechnology(tech, {
-      alertMessage: 'Ready to read an NFC tag'
-    });
-    const ndef = await NfcManager.getNdefMessage();
-    if (ndef) {
-      const id = ndef[0].payload;
-      setArtworkID(id);
-      handlePress();
-    }
-    await NfcManager.closeTechnology(tech);
-  };
+  // // Handle scanned NFC tag
+  // const handleNfcDiscover = async (tag) => {
+  //   const tech = NfcTech.Ndef;
+  //   const result = await NfcManager.requestTechnology(tech, {
+  //     alertMessage: 'Ready to read an NFC tag'
+  //   });
+  //   const ndef = await NfcManager.getNdefMessage();
+  //   if (ndef) {
+  //     const id = ndef[0].payload;
+  //     setArtworkID(id);
+  //     handlePress();
+  //   }
+  //   await NfcManager.closeTechnology(tech);
+  // };
 
-  // Enable foreground dispatch for NFC tag scanning
-  useEffect(() => {
-    const enableForegroundDispatch = async () => {
-      try {
-        await NfcManager.registerTagEvent(handleNfcDiscover);
-      } catch (ex) {
-        console.warn('Error enabling NFC foreground dispatch', ex);
-      }
-    };
-    enableForegroundDispatch();
-    return () => {
-      NfcManager.unregisterTagEvent().catch(() => 0);
-    };
-  }, []);
+  // // Enable foreground dispatch for NFC tag scanning
+  // useEffect(() => {
+  //   const enableForegroundDispatch = async () => {
+  //     try {
+  //       await NfcManager.registerTagEvent(handleNfcDiscover);
+  //     } catch (ex) {
+  //       console.warn('Error enabling NFC foreground dispatch', ex);
+  //     }
+  //   };
+  //   enableForegroundDispatch();
+  //   return () => {
+  //     NfcManager.unregisterTagEvent().catch(() => 0);
+  //   };
+  // }, []);
 
-  if (!fontLoaded) {
-    return null;
-  }
+  // if (!fontLoaded) {
+  //   return null;
+  // }
 
   return (
     <View style={styles.container}>
@@ -83,7 +83,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     marginBottom: 40,
     color: '#E4A21D',
-    fontFamily: 'Helvetica Neue',
+    fontFamily: 'System',
     letterSpacing: 2,
   },
   button: {
